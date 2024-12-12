7fbd0347200b4b6536af61195c263f003f20208d
Mohammed Misran <mohammed.misran@pitt.edu>
2024-12-12 16:49:21 -0500

    refactored spotify service

---
+++ verse-vibe-backend/src/main/java/edu/isr/versevibe/dto/spotify/ExternalUrls.java
@@ -0,0 +1,10 @@
+package edu.isr.versevibe.dto.spotify;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExternalUrls {
+    private String spotify;
+}
---
+++ verse-vibe-backend/src/main/java/edu/isr/versevibe/dto/spotify/SpotifySearchResponse.java
@@ -0,0 +1,10 @@
+package edu.isr.versevibe.dto.spotify;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SpotifySearchResponse {
+    private SpotifySearchTracksResponse tracks;
+}
