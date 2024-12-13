e5dac40a4f7800ccb964892898a262d16e5913c1
Mohammed Misran <mohammed.misran@pitt.edu>
2024-12-13 00:54:17 -0500

    updated song mapper

--- verse-vibe-backend/src/main/java/edu/isr/versevibe/dto/spotify/SpotifyArtist.java
+++ verse-vibe-backend/src/main/java/edu/isr/versevibe/dto/spotify/SpotifyArtist.java
@@ -3,9 +3,11 @@ package edu.isr.versevibe.dto.spotify;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class SpotifyArtist {
     private String id;
     private String name;
--- verse-vibe-backend/src/main/java/edu/isr/versevibe/mapper/SongMapper.java
+++ verse-vibe-backend/src/main/java/edu/isr/versevibe/mapper/SongMapper.java
@@ -6,22 +6,34 @@ import edu.isr.versevibe.dto.result.Artist;
 import edu.isr.versevibe.dto.result.SongResult;
 import edu.isr.versevibe.dto.spotify.SpotifyArtist;
 import edu.isr.versevibe.dto.spotify.SpotifySearchResponse;
+import edu.isr.versevibe.dto.spotify.SpotifyTrack;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SongMapper {
     public static SongResult toSongResult(final Song song, final SpotifySearchResponse spotifySearchResponse) {
         final SongResult songResult = new SongResult();
-        songResult.setElasticSearchId(song.getElasticsearchId());
-        songResult.setName(spotifySearchResponse.getTracks().getItems().get(0).getName());
-        populateAlbumData(songResult, spotifySearchResponse);
-        songResult.setExplicit(spotifySearchResponse.getTracks().getItems().get(0).getExplicit());
-        populateArtistData(songResult, song, spotifySearchResponse);
-        populateDuration(songResult, spotifySearchResponse.getTracks().getItems().get(0).getDurationInMilliseconds());
         songResult.setDocId(song.getDocId());
-        songResult.setSpotifyId(spotifySearchResponse.getTracks().getItems().get(0).getId());
+        songResult.setElasticSearchId(song.getElasticsearchId());
+
+        if (spotifySearchResponse.getTracks().getItems().isEmpty()) {
+            songResult.setName(song.getTitle());
+            populateAlbumData(songResult, song);
+            populateArtistData(songResult, song);
+        } else {
+            SpotifyTrack track = spotifySearchResponse.getTracks().getItems().get(0);
+            songResult.setSpotifyId(track.getId());
+            songResult.setName(track.getName());
+            songResult.setSpotifyUrl(track.getExternalUrls().getSpotify());
+            songResult.setExplicit(track.getExplicit());
+
+            populateAlbumData(songResult, track);
+            populateArtistData(songResult, track);
+            populateDuration(songResult, track.getDurationInMilliseconds());
+        }
         return songResult;
     }
 
@@ -33,24 +45,43 @@ public class SongMapper {
         songResult.setDuration(durationString);
     }
 
-    private static void populateArtistData(final SongResult songResult, final Song song,
-                                           final SpotifySearchResponse spotifySearchResponse) {
+    private static void populateArtistData(final SongResult songResult, final SpotifyTrack track) {
         final List<Artist> artists = new ArrayList<>();
-        for (SpotifyArtist spotifyArtist : spotifySearchResponse.getTracks().getItems().get(0).getArtists()) {
+        for (SpotifyArtist spotifyArtist : track.getArtists()) {
             final Artist artist = new Artist();
-            artist.setName(spotifyArtist.getName());
             artist.setSpotifyId(spotifyArtist.getId());
+            artist.setName(spotifyArtist.getName());
             artist.setSpotifyUrl(spotifyArtist.getExternalUrls().getSpotify());
             artists.add(artist);
-            songResult.setArtists(artists);
         }
+        songResult.setArtists(artists);
     }
 
-    private static void populateAlbumData(final SongResult songResult,
-                                          final SpotifySearchResponse spotifySearchResponse) {
+    private static void populateArtistData(final SongResult songResult, final Song song) {
+        List<Artist> artists = IntStream.range(0, song.getArtists().size()).mapToObj(i -> {
+            Artist artist = new Artist();
+            artist.setSpotifyId(String.valueOf(i + 1));
+            artist.setName(song.getArtists().get(i));
+            return artist;
+        }).toList();
+
+        songResult.setArtists(artists);
+    }
+
+    private static void populateAlbumData(final SongResult songResult, final SpotifyTrack track) {
         final Album album = new Album();
-        album.setName(spotifySearchResponse.getTracks().getItems().get(0).getAlbum().getName());
-        album.setImages(spotifySearchResponse.getTracks().getItems().get(0).getAlbum().getImages());
+        album.setSpotifyId(track.getAlbum().getId());
+        album.setName(track.getAlbum().getName());
+        album.setSpotifyUrl(track.getAlbum().getExternalUrls().getSpotify());
+        album.setReleaseDate(track.getAlbum().getReleaseDate().substring(0, 4));
+        album.setImages(track.getAlbum().getImages());
+        songResult.setAlbum(album);
+    }
+
+    private static void populateAlbumData(final SongResult songResult, Song song) {
+        final Album album = new Album();
+        album.setName("Unknown Album");
+        album.setReleaseDate(song.getYear());
         songResult.setAlbum(album);
     }
 }
